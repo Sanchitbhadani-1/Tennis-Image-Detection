23bb53e3525f7d850e929bf9b6444339d3ebcbd5
Sanchitbhadani-1 <santheman40@gmail.com>
2026-06-25 17:01:46 -0400

    Testing YOLO

---
+++ detect_image.py
@@ -0,0 +1,13 @@
+from ultralytics import YOLO
+
+model = YOLO("yolov8n.pt")
+results = model("https://ultralytics.com/images/bus.jpg")
+result = results[0]
+
+for box in result.boxes:
+    class_id = int(box.cls[0])
+    confidence = float(box.conf[0])
+    name = model.names[class_id]
+    print(name, round(confidence, 2))
+
+result.save(filename="output.jpg")
